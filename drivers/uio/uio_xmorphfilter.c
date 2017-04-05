6dbf391f9bd88229447b88929dcfc5d9d05666fd
wafgo <wadim.mueller@gmx.de>
2017-04-05 21:37:14 +0200

    add GPL Licese head

--- drivers/uio/uio_xmorphfilter.c
+++ drivers/uio/uio_xmorphfilter.c
@@ -1,9 +1,20 @@
 /*
  * uio_xmorphfilter.c
  *
- *  Created on: 06.03.2017
- *      Author: wadim mueller
+ * Copyright (C) 2017 Wadim Mueller
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 
 #include "linux/platform_device.h"
 #include <linux/slab.h>
